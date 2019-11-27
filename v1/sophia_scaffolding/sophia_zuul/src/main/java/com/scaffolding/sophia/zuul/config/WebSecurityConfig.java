1336b786b5d00ec262fdb5f4f62cb61d9e23cde0
lihuilin <l>
2019-11-27 19:07:34 +0800

    修改公共包

--- v1/sophia_scaffolding/sophia_zuul/src/main/java/com/scaffolding/sophia/zuul/config/WebSecurityConfig.java
+++ v1/sophia_scaffolding/sophia_zuul/src/main/java/com/scaffolding/sophia/zuul/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.scaffolding.sophia.zuul.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
