f78b8e2caa090a3e4dd2783f59091fd946b8d410
lihuilin <1054868512@qq.com>
2020-05-29 10:40:39 +0800

    修改v3

--- v3/sophia_scaffolding/sophia_common/sophia_common_security/src/main/java/com/scaffolding/sophia/common/security/util/UserUtils.java
+++ v3/sophia_scaffolding/sophia_common/sophia_common_security/src/main/java/com/scaffolding/sophia/common/security/util/UserUtils.java
@@ -1,7 +1,10 @@
 package com.scaffolding.sophia.common.security.util;
 
 import com.scaffolding.sophia.common.security.model.LoginUser;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 /**
  * @author: LHL
@@ -15,6 +18,19 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class UserUtils {
 
     public static LoginUser getLoginUser() {
-        return (LoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //判断是否可以转换
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof OAuth2Authentication) {
+            OAuth2Authentication oAuth2Auth = (OAuth2Authentication) authentication;
+            authentication = oAuth2Auth.getUserAuthentication();
+            if (authentication instanceof UsernamePasswordAuthenticationToken) {
+                UsernamePasswordAuthenticationToken authenticationToken = (UsernamePasswordAuthenticationToken) authentication;
+                // Map map = (Map) authenticationToken.getDetails();
+                // map = (Map) map.get("principal");
+                // return JSONObject.parseObject(JSONObject.toJSONString(map), LoginUser.class);
+                return (LoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            }
+        }
+        return null;
     }
 }
