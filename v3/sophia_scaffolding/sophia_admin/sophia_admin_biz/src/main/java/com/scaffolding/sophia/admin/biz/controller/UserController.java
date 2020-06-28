bdc68f1ae6ca1091b2ba7899f0fe177ed9408d89
lihuilin <1054868512@qq.com>
2020-06-28 11:54:40 +0800

    用户新增编辑

--- v3/sophia_scaffolding/sophia_admin/sophia_admin_biz/src/main/java/com/scaffolding/sophia/admin/biz/controller/UserController.java
+++ v3/sophia_scaffolding/sophia_admin/sophia_admin_biz/src/main/java/com/scaffolding/sophia/admin/biz/controller/UserController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
